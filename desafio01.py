2c0789151d9c3710885d962216b8d6dc6aea013f
Pedro Lima <pedrolimaa354@gmail.com>
2024-11-24 23:04:38 -0300

    Checkpoint: Lista de exercícios adicionada.

--- desafio01.py
+++ desafio01.py
@@ -1,1 +1,15 @@
-print("Teste do repositório")
+'''
+1. Imprima a frase Escola de Dados da Alura!.
+2. Imprima seu nome e seu sobrenome seguindo a estrutura abaixo:
+3. Imprima o seu primeiro nome letra a letra. Por exemplo, meu nome é Mirla, então eu obtenho a seguinte saída:
+    M
+    I
+    R
+    L
+    A
+4. Imprima o dia do seu nascimento em formato dia mês ano. Lembrando que os valores de dia e ano não podem estar entre aspas. Supondo uma data de aniversário dia 28 de fevereiro de 2003, o formato deve estar como no exemplo abaixo:
+    28 fevereiro 2003
+
+5. Imprima, em um único print, o atual ano que você está fazendo esse curso. O valor do ano deve ser um dado numérico e a saída do print deve ser a seguinte:
+    Ano atual: [ano]
+'''
