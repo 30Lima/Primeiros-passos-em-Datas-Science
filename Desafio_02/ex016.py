96181de80a84e24d5b528e99c571c3b8ddfc17fe
Pedro Lima <pedrolimaa354@gmail.com>
2024-12-05 16:48:23 -0300

    Exercício 016 finalizado.

---
+++ Desafio_02/ex016.py
@@ -0,0 +1,6 @@
+#Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase digitada mas com todas as letras minúsculas.
+
+frase_usuario = input("Digite uma frase: ")
+
+frase_minuscula = str.lower(frase_usuario)
+print(frase_minuscula)
