39cb4a1fed38f466a416422a1491d59ad5956721
Pedro Lima <pedrolimaa354@gmail.com>
2025-01-23 22:14:59 -0300

    Exercício finalizado.

--- Desafio_03/desafio03.py
+++ Desafio_03/desafio03.py
@@ -1,16 +1,11 @@
-#Escreva um programa que leia valores médios de preços de um modelo de carro por 3 anos 
-#consecutivos e exiba o valor mais alto e mais baixo entre esses três anos.
+#3) Escreva um programa que determine se uma letra fornecida pela pessoa usuária é uma vogal ou consoante.
 
 import os
 os.system("cls")
 
-preco_medio_um = float(input("Digite o preço do carro no primeiro ano: "))
-preco_medio_dois = float(input("Digite o preço do carro no segundo ano: "))
-preco_medio_tres = float(input("Digite o preço do carro no terceiro ano: "))
+letra_usuario = str(input("Digite uma letra do afabeto: "))
 
-if preco_medio_um > preco_medio_dois and preco_medio_tres:
-    print("Em comparação aos outros, no primeiro ano o carro estava com um valor mais alto.")
-elif preco_medio_dois > preco_medio_um and preco_medio_tres:
-    print("Em comparação aos outros, no segundo ano o carro estava com um valor mais alto.")
-else:
-    print("Em comparação aos outros, no terceiro ano o carro estava com um valor mais alto.")
+if letra_usuario == "A" or "E" or "I" or "o" or "u":
+    print("A letra digitada é uma vogal!")
+else: 
+    print("A letra digitada é uma consoante!")
