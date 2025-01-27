887728847151ae420546e99e61bd5c40d9a367c9
Pedro Lima <pedrolimaa354@gmail.com>
2025-01-26 23:01:05 -0300

    Exercício finalizado.

---
+++ Desafio_04/desafio06.py
@@ -0,0 +1,4 @@
+#6) Escreva um programa que gere a tabuada de um número inteiro de 1 a 10, de acordo com a escolha da pessoa usuária. 
+# Como exemplo, para o número 2, a tabuada deve ser mostrada no seguinte formato:
+
+
