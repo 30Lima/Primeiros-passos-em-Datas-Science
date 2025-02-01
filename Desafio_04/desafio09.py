46351e7a6542d9fe65d3b444b38b71915162d9b5
Pedro Lima <pedrolimaa354@gmail.com>
2025-02-01 00:52:43 -0300

    Finish: Código finalizado.

--- Desafio_04/desafio09.py
+++ Desafio_04/desafio09.py
@@ -7,7 +7,6 @@
 # Além disso, deve calcular e exibir a porcentagem de votos nulos em relação ao total de votos e a porcentagem de votos em 
 # branco em relação ao total de votos.
 
-
 import os
 os.system("cls")
 
@@ -19,24 +18,82 @@ nulo = 0
 branco = 0 
 
 for i in range(1,21):
-    voto_usuario = int(input("Digite o número do candidato: "))
-    while voto_usuario >= 7 and voto_usuario <= 0:
-          print("Por favor, digite um número válido.")
+      while True:
+         try:
+             voto_usuario = int(input("Digite o número do candidato: "))
+             if voto_usuario in [1,2,3,4,5,6]:
+                  break
+             print("Por gentileza, digite um número válido.")
+         except ValueError:
+              print("Por gentileza, digite um número válido.")
 
-    if voto_usuario == 1:
+      if voto_usuario == 1:
        primeiro_candidato += 1
-    elif voto_usuario == 2:
-       segundo_candidato += 1
-    elif voto_usuario == 3:
+      elif voto_usuario == 2:
+        segundo_candidato += 1
+      elif voto_usuario == 3:
        terceiro_candidato += 1
-    elif voto_usuario == 4:
+      elif voto_usuario == 4:
         quarto_candidato += 1
-    elif voto_usuario == 5:
+      elif voto_usuario == 5:
        nulo += 1
-    elif voto_usuario == 6:
+      elif voto_usuario == 6:
        branco += 1
-    break
 
-print(f"{primeiro_candidato}")
+if primeiro_candidato > max(segundo_candidato, terceiro_candidato, quarto_candidato):
+   mensagem = "\n".join ([
+         (f"""QUANTIDADE DE VOTOS POR CANDIDATO:,
+          Primeiro candidato: {primeiro_candidato}
+          Segundo candidato: {segundo_candidato}
+          Terceiro candidato: {terceiro_candidato}
+          Quarto candidato: {quarto_candidato}
+          Votos nulos: {nulo}
+          Votos em branco: {branco}
+          O primeiro candidato venceu as eleições com {primeiro_candidato} votos!""")
+   ])
+   print(mensagem)
+
+elif segundo_candidato > max(primeiro_candidato, terceiro_candidato, quarto_candidato):
+   print(f"""QUANTIDADE DE VOTOS POR CANDIDATO: \n Primeiro candidato: {primeiro_candidato} 
+         Segundo candidato: {segundo_candidato} \n Terceiro candidato: {terceiro_candidato} 
+         Quarto candidato: {quarto_candidato} \n Votos nulos: {nulo}\n Votos em branco: {branco}\n 
+         O segundo candidato venceu as eleições com {segundo_candidato} votos!""")
+
+elif terceiro_candidato > max(primeiro_candidato, segundo_candidato, terceiro_candidato, quarto_candidato):
+   print(f"""QUANTIDADE DE VOTOS POR CANDIDATO: \n Primeiro candidato: {primeiro_candidato} 
+         Segundo candidato: {segundo_candidato} \n Terceiro candidato: {terceiro_candidato} 
+         Quarto candidato: {quarto_candidato} \n Votos nulos: {nulo}\n Votos em branco: {branco}\n +
+         O terceiro candidato venceu as eleições com {terceiro_candidato} votos!""")
+   
+elif quarto_candidato > max(primeiro_candidato, segundo_candidato, terceiro_candidato):
+    print(f"""QUANTIDADE DE VOTOS POR CANDIDATO: \n Primeiro candidato: {primeiro_candidato} 
+         Segundo candidato: {segundo_candidato} \n Terceiro candidato: {terceiro_candidato} 
+         Quarto candidato: {quarto_candidato} \n Votos nulos: {nulo}\n Votos em branco: {branco}\n 
+         O quarto candidato venceu as eleições com {quarto_candidato} votos!""")
     
+
+
+if (primeiro_candidato == segundo_candidato == terceiro_candidato == quarto_candidato):
+    print("Empate entre todos os candidatos!")
+elif primeiro_candidato == segundo_candidato:
+    print("Empate entre o primeiro e o segundo candidato!")
+elif primeiro_candidato == terceiro_candidato:
+    print("Empate entre o primeiro e o terceiro candidato!")
+elif primeiro_candidato == quarto_candidato:
+    print("Empate entre o primeiro e o quarto candidato!")
+elif segundo_candidato == terceiro_candidato:
+    print("Empate entre o segundo e o terceiro candidato!")
+elif segundo_candidato == quarto_candidato:
+    print("Empate entre o segundo e o quarto candidato!")
+elif terceiro_candidato == quarto_candidato:
+    print("Empate entre o terceiro e o quarto candidato!")
+
+soma_votos = primeiro_candidato + segundo_candidato + terceiro_candidato + quarto_candidato + nulo + branco
+
+if soma_votos > 0:
+   porcentagem_nulos = (nulo / soma_votos) * 100
+   porcentagem_brancos = (branco / soma_votos) * 100
+   print("")
+   print(f"Porcentagem de votos nulos: {porcentagem_nulos:.2f}%")
+   print(f"Porcetame de votos em branco: {porcentagem_brancos:.2f}%")
 
